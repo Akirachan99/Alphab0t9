2f954c29b11a1fb608d8c3fcc3588afec8227546
Uchiha177 <67803612+Uchiha177@users.noreply.github.com>
2022-01-14 07:29:59 +0700

    Update and rename shop/allpayment.js to toko/allpayment.js

---
+++ toko/allpayment.js
@@ -0,0 +1,20 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY ZEEONE OFC
+
+*さ E-MONEY*
+1. GOPAY
+	ㅁ 0813 2732 7914
+		• MO***** F***R
+2. OVO
+	ㅁ 0813 2732 7914
+		• MO***** F***R
+3. DANA
+	ㅁ 0813 2732 7914
+		• MO***** F***R
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
